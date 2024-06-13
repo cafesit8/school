f0241c9426759b29defa5da2b6fdde7de3a1031e
leliasgomer <leliasgomer@gmail.com>
2024-06-13 17:15:59 -0500

    improve the way to show images

---
+++ src/data/Images.ts
@@ -0,0 +1,56 @@
+export const images = [
+  {
+    href: '/img1.webp',
+    width: 1600,
+    height: 1600,
+    label: 'Imagen de la subdirectora del colegio',
+  },
+  {
+    href: '/img5.webp',
+    width: 1600,
+    height: 1200,
+    label: 'Imagen del aula de computación',
+  },
+  {
+    href: '/img2.webp',
+    width: 1600,
+    height: 1600,
+    label: 'Alumnos en la formación',
+  },
+  {
+    href: '/img3.webp',
+    width: 1600,
+    height: 1600,
+    label: 'Conferencia en el auditorio',
+  },
+  {
+    href: '/img4.webp',
+    width: 1600,
+    height: 1600,
+    label: 'Alumnas en formación',
+  },
+  {
+    href: '/img6.webp',
+    width: 1600,
+    height: 1204,
+    label: 'Aula de computación',
+  },
+  {
+    href: '/teachers.webp',
+    width: 1600,
+    height: 1204,
+    label: 'Profesores',
+  },
+  {
+    href: '/students.webp',
+    width: 700,
+    height: 322,
+    label: 'Alumnos del colegio Carlos Cueto Fernandini',
+  },
+  {
+    href: '/direction.jpg',
+    width: 1600,
+    height: 1200,
+    label: 'Direccion',
+  }
+]

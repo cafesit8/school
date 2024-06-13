232c44dab09950a9809dfdd2ae875ec17b31ad62
leliasgomer <leliasgomer@gmail.com>
2024-06-13 18:28:08 -0500

    add rules section

---
+++ src/data/normas.ts
@@ -0,0 +1,74 @@
+export const normas = [
+  {
+    img: '/puntualidad.jpg',
+    width: 450,
+    height: 368,
+    alt: 'Imagen de puntualidad',
+    subtitle: 'Puntualidad',
+    text: 'Demostramos puntualidad al ingreso a la I.E., y al aula, así como en el desarrollo de diversas actividades.'
+  },
+  {
+    img: '/aseo_personal.jpg',
+    width: 612,
+    height: 306,
+    alt: 'Aseo personal',
+    subtitle: 'Aseo',
+    text: 'Asistimos limpios y con el uniforme escolar a la I.E., promoviendo la salud física.'
+  },
+  {
+    img: '/convivencia.jpeg',
+    width: 277,
+    height: 182,
+    alt: 'Una buena convivencia',
+    subtitle: 'Buen Trato',
+    text: 'Practicamos buen trato, convivencia sana, empatía y tolerancia, respetando las diferencias sin distinción alguna, fortaleciendo la salud emocional.'
+  },
+  {
+    img: '/computacion.jpg',
+    width: 600,
+    height: 479,
+    alt: 'Niños en el aula de computación',
+    subtitle: 'Medios Virutales',
+    text: 'Utilizamos adecuadamente los medios virtuales (práctica de la netiqueta), para fortalecer nuestra identidad y ciudadanía digital.'
+  },
+  {
+    img: '/cuidado.webp',
+    width: 600,
+    height: 447,
+    alt: 'Comunicación escolar',
+    subtitle: 'Comunicación',
+    text: 'Nos comunicamos en horarios establecidos, toda la comunidad educativa.'
+  },
+  {
+    img: '/prevencion.webp',
+    width: 450,
+    height: 368,
+    alt: 'Niños atendiendo a la maestra',
+    subtitle: 'Prevención',
+    text: 'Participamos en acciones de prevención sobre desastres naturales y acciones del cuidado del ambiente.'
+  },
+  {
+    img: '/clase.webp',
+    width: 626,
+    height: 402,
+    alt: 'Niños en clase',
+    subtitle: 'Respeto',
+    text: 'Respetamos horarios, tiempos y formas establecidas en las reuniones, clases y diferentes actividades institucionales.'
+  },
+  {
+    img: '/cuidamos_lo_nuestro.webp',
+    width: 626,
+    height: 626,
+    alt: 'Niños con sus materiales',
+    subtitle: 'Cuidado Ajeno',
+    text: 'Cuidamos los bienes materiales haciéndonos responsables del deterioro que podamos ocasionar en nuestra I.E. y respetamos lo ajeno.'
+  },
+  {
+    img: '/perseverancia.webp',
+    width: 600,
+    height: 400,
+    alt: 'Niños en clase',
+    subtitle: 'Perseverancia',
+    text: 'Actuamos con perseverancia para el logro de los aprendizajes y proyecto de vida haciendo uso de diversas herramientas de estudio.'
+  }
+]
